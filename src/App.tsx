30e742ab2c70d18c45062bfc30ee40445693e9a1
elton de campos <eltoncampos36@gmail.com>
2021-04-21 02:26:28 -0300

    chore: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from "styled-components";
+
 import { GlobalStyle } from "./styles/global";
 import { Layout } from "./components/Layout";
+import dark from "./styles/themes/dark";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyle />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 }
