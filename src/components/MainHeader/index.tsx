cf41f3aaa2bd4ec4a7dcb97ff4e459a0b2e81084
elton de campos <eltoncampos36@gmail.com>
2021-04-21 02:47:03 -0300

    chore: make emojis return dinamic

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,22 @@
+import { useMemo } from "react";
 import * as S from "./styles";
 
+import emojis from "../../utils/emojis";
+
 export function MainHeader() {
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * emojis.length);
+    return emojis[index];
+  }, []);
+
   return (
     <S.Container>
-      <h1>MainHeader</h1>
+      <h1>toggle</h1>
+
+      <S.Profile>
+        <S.Welcome>Olá, {emoji}</S.Welcome>
+        <S.UserName>Elton Campos</S.UserName>
+      </S.Profile>
     </S.Container>
   );
 }
