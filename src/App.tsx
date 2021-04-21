45c6ec120c5981df83416b90e27d13e65503a1bd
elton de campos <eltoncampos36@gmail.com>
2021-04-21 01:33:56 -0300

    chore: add fonts and create global styles

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+import { Dashboard } from "./pages/Dashboard";
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Dashboard />
+    </>
+  );
+}
