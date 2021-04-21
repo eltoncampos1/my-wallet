f66934e13c9641cfa68081ee46926c016b7cfe9a
elton de campos <eltoncampos36@gmail.com>
2021-04-21 02:26:42 -0300

    chore: create light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default  {
+    title: 'light',
+
+    colors: {
+        primary: '#dcdcdc',
+        secondary: '#fff',
+        tertiary: '#f5f5f5',
+
+
+        white: '#000',
+        black: '#fff',
+        gray: '#bfbfbf',
+
+        success: '#03b885',
+        info: '#3b5998',
+        warning: '#ff6961',
+    },
+}
