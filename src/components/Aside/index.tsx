a443ad215f485aae96fde662475c1ff362dd73bf
elton de campos <eltoncampos36@gmail.com>
2021-04-21 21:35:38 -0300

    chore: create aside bar

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,9 +1,40 @@
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from "react-icons/md";
+
 import * as S from "./styles";
+
+import logo from "../../assets/logo.svg";
 
 export function Aside() {
   return (
     <S.Container>
-      <h1>Aside</h1>
+      <S.Header>
+        <S.LogoImg src={logo} />
+        <S.Title>My Wallet</S.Title>
+      </S.Header>
+
+      <S.MenuContainer>
+        <S.MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdArrowDownward />
+          Saidas
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </S.MenuItemLink>
+      </S.MenuContainer>
     </S.Container>
   );
 }
